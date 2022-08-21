a301c9a3c2c1795503f2725d16d8a1f64fc64125
PALRAJ BALASAINATH <110807027+Balasainath17@users.noreply.github.com>
2022-08-21 16:49:10 +0530

    Add files via upload

---
+++ Assignment_4.c
@@ -0,0 +1,151 @@
+// 1. Write a program to print MySirG 5 times on the screen
+/*
+#include<stdio.h>
+int main()
+{
+    int i=1;
+    while (i<=5)
+    {
+        printf("MySirG\n");
+        i++;
+    }
+    return 0;
+}
+*/
+
+// 2. Write a program to print the first 10 natural numbers.
+/*
+#include<stdio.h>
+int main()
+{
+    int i=1;
+    while (i<=10)
+    {
+        printf("%d\t",i);
+        i++;
+    }
+    return 0;
+}
+*/
+
+// 3. Write a program to print the first 10 natural numbers in reverse order
+/*
+#include<stdio.h>
+int main()
+{
+    int i=10;
+    while (i>=1)
+    {
+        printf("%d\n",i);
+        i--;
+    }
+    return 0;
+}
+*/
+
+// 4. Write a program to print the first 10 odd natural numbers
+/*
+#include<stdio.h>
+int main()
+{
+    int n=1;
+    while (n<=10)
+    {
+        printf("%d\n",2*n-1);
+        n++;
+    }
+    return 0;
+}
+*/
+
+// 5. Write a program to print the first 10 odd natural numbers in reverse order.
+/*
+#include<stdio.h>
+int main()
+{
+    int n=10;
+    while (n>=1)
+    {
+        printf("%d\n",2*n-1);
+        n--;
+    }
+    return 0;
+}
+*/
+
+// 6. Write a program to print the first 10 even natural numbers
+/*
+#include<stdio.h>
+int main()
+{
+    int n=1;
+    while (n<=10)
+    {
+        printf("%d\n",2*n);
+        n++;
+    }
+    return 0;
+}
+*/
+
+// 7. Write a program to print the first 10 even natural numbers in reverse order
+
+/*
+#include<stdio.h>
+int main()
+{
+    int n=10;
+    while (n>=1)
+    {
+        printf("%d\n",2*n);
+        n--;
+    }
+    return 0;
+}
+*/
+
+// 8. Write a program to print squares of the first 10 natural numbers
+/*
+#include<stdio.h>
+int main()
+{
+    int n=1;
+    while (n<=10)
+    {
+        printf("%d\n",n*n);
+        n++;
+    }
+    return 0;
+}
+*/
+
+// 9. Write a program to print cubes of the first 10 natural numbers
+
+/*
+#include<stdio.h>
+int main()
+{
+    int n=1;
+    while (n<=10)
+    {
+        printf("%d\n",n*n*n);
+        n++;
+    }
+    return 0;
+}
+*/
+
+// 10. Write a program to print a table of 5.
+/*
+#include<stdio.h>
+int main()
+{
+    int x,y;
+    for(x=1;x<=10;x++)
+    {
+        y=5*x;
+        printf("5*%d=%d\n",x,y);
+    }
+    return 0;
+}
+*/
